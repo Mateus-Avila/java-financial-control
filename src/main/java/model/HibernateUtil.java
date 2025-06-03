8e43148693236db85a54f909cb2d6f6a3a04497c
Mateus <mateusmenavila@gmail.com>
2025-06-02 22:32:29 -0300

    General Adjusts

--- src/main/java/model/HibernateUtil.java
+++ src/main/java/model/HibernateUtil.java
@@ -2,15 +2,20 @@ package model;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
     private static final SessionFactory sessionFactory;
 
     static {
         try {
-            sessionFactory = new Configuration()
-                    .configure() // carrega hibernate.cfg.xml automaticamente
-                    .buildSessionFactory();
+            Configuration configuration = new Configuration().configure(); // Lê o hibernate.cfg.xml
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         } catch (Throwable ex) {
             System.err.println("Falha na criação do SessionFactory: " + ex);
             throw new ExceptionInInitializerError(ex);
